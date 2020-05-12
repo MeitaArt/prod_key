afa83792bc15e34f6b2473f6e4d5516ab8ba1834
meitaart <meitaartem@gmail.com>
2020-05-12 14:37:54 +0300

    upload method stub

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    pass
